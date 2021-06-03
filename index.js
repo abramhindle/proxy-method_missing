4dd22cbfff930312ea0211b03605d077ee2becd8
Abram Hindle <abram.hindle@softwareprocess.es>
2021-06-03 16:38:34 -0600

    Presentation rendering and a good thrust

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
 class Example {
-	a$number$number(x,y) {
-		return x + y
-	}
-	a$string$number(s,n) {
-		return [...Array(n)].map(() => s).join("")
-	}
-	// a() {
-	// 	let prefix = "a_";	
-	// 	let sig = prefix + [...arguments].map(x => typeof(x)).join("_");
-	// 	return this[sig](...arguments);
-	// }
-}
-function argsMatchDef(def, args) {
-
+    a$number$number(x,y) {
+	return x + y
+    }
+    a$string$number(s,n) {
+	return [...Array(n)].map(() => s).join("")
+    }
 }
 function makeSimpleMultiMethod(prefix) {
-	return function() {
+    return function() {
 		let sig = prefix + "$" + [...arguments].map(x => typeof(x)).join("$");
-		if (sig in this) {
-			return this[sig](...arguments);
-		} else {
-			throw (sig + ' not in ' + this)
-		}
+	if (sig in this) {
+	    return this[sig](...arguments);
+	} else {
+	    throw (sig + ' not in ' + this)
 	}
+    }
 }
-
 function handlerGet(target, prop, receiver) {
-	if (prop in target) {
-		return Reflect.get(...arguments);
-	}
-	target[prop] = makeSimpleMultiMethod(prop);
-	return Reflect.get(...arguments);
+    props = prop.toString();
+    if (props in target) {
+        return Reflect.get(target,prop,receiver);
+    }
+    target[props] = makeSimpleMultiMethod(prop);
+    return Reflect.get(target, prop, receiver);
 }
 function makeHandler() {
-	return {
-		get: handlerGet
-	}
+    return {
+	get: handlerGet
+    }
 }
+// We can wrap it with proxy
 pe = new Proxy(new Example(),makeHandler());
-pe.a(1,2)
-pe.a("x",5)
-pe.a("x","x")
-Example.prototype.a = makeSimpleMultiMethod("a")
-e = new Example()
-e.a(1,2)
-e.a("x",5)
-e.a("x","x")
+console.log( "pe.a(1,2)    "     ,       pe.a(1,2)    );
+console.log( "pe.a(\"x\",5)  "   ,       pe.a("x",5)  );
+try {
+    console.log( "pe.a(\"x\",\"x\")" ,       pe.a("x","x"));
+} catch (err) {
+    console.log("Error "+err);
+}
+// We can forget proxy and just make the method a multi method by default
+Example.prototype.a = makeSimpleMultiMethod("a");
+e = new Example();
+console.log( "e.a(1,2)    "     ,       e.a(1,2)    );
+console.log( "e.a(\"x\",5)  "   ,       e.a("x",5)  );
+try {
+    console.log( "e.a(\"x\",\"x\")" ,       e.a("x","x"));
+} catch (err) {
+    console.log("Error "+err);
+}
 
